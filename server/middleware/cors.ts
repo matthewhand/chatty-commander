1a27d1df5514bd5d52915f68fd20778fbb8c7050
Matthew Hand <matthewhandau@gmail.com>
2026-02-26 18:43:22 +0000

    chore: purge all remaining legacy frontend, server, and worker files

--- server/middleware/cors.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-
-export function corsHeaders(req: Request, res: Response, next: NextFunction) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  next();
-}
